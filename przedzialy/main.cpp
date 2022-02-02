d65227b513d077a4a8b7be76383cba259ed745d8
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-02 09:55:06 +0100

    Initial Commit

---
+++ przedzialy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  przedzialy
+//
+//  Created by Kuba Wtorkiewicz on 02/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
