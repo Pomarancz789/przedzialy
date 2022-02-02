fa3bec547216280214756cf634f813a99b95d23c
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-02 10:43:40 +0100

    popełniam bisekcję, gówno działa, ale tylko dla tego konkretnego wzoru funkcji

--- przedzialy/main.cpp
+++ przedzialy/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  przedzialy
-//
-//  Created by Kuba Wtorkiewicz on 02/02/2022.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+float f(float x){
+    return x * x - 4;
+}
+
+int bisekcja(int a, int b, float d)
+{
+    if(f(a) == 0.0)
+        return a;
+    if(f(b) == 0.0)
+        return b;
+    
+    int s;
+    
+    while(b - a > d)
+    {
+        s = (a + b) / 2;
+        if(f(s) == 0.0f)
+            return s;
+        if(f(a) * f(s) < 0) b = s;
+        else a = s;
+    }
+    return (a+b) / 2;
+}
+
+int main(){
+    int a = 1, b = 5 ;
+    float d = 0.01;
+    
+    cout<<bisekcja(a,b,d)<<endl;
 }
